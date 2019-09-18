a21438bf4a4b36aa8d0d0a8e5e0583fb550da84e
Frederic Madesta <dericdesta@users.noreply.github.com>
2019-09-18 15:07:05 +0200

    Update setup.py
    removed long_description due to ascii error

--- setup.py
+++ setup.py
@@ -19,9 +19,6 @@
 
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-	long_description = fh.read()
-
 setup(
 	name='petname',
 	description='Generate human-readable, random object names',
